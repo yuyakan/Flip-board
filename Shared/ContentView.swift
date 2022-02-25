6cdd6f191c156d7e6edebe5165d35d8f459e2c76
yuyakan <kanbe1365@icloud.com>
2022-02-25 15:38:02 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 上別縄祐也 on 2022/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Flip_boardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flip_boardApp.swift
+//  Shared
+//
+//  Created by 上別縄祐也 on 2022/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Flip_boardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
